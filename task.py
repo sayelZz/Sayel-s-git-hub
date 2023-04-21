cde79ad46165677fadb522363f6acdb7eb6c01cc
sayelZz <130446710+sayelZz@users.noreply.github.com>
2023-04-21 06:58:50 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+name = 'sayel'
+
+
+# In[4]:
+
+
+name
+
+
+# In[6]:
+
+
+x = 'hello world'
+
+
+# In[7]:
+
+
+x
+
+
+# In[11]:
+
+
+x +' it is beautiful day outside'
+
+
+# In[12]:
+
+
+letter ='Z'
+
+
+# In[14]:
+
+
+letter*3
+
+
+# In[16]:
+
+
+'40'+'40'
+
+
+# In[19]:
+
+
+x=x.upper()
+
+
+# In[20]:
+
+
+x
+
+
+# In[21]:
+
+
+x=x.lower()
+
+
+# In[22]:
+
+
+x
+
+
+# In[23]:
+
+
+x.split()
+
+
+# In[24]:
+
+
+x.split('o')
+
+
+# In[25]:
+
+
+'''
+Hello there !
+
+'''
+
+
+# In[28]:
+
+
+my_name = 'sayel'
+print('hello my name is '+my_name)
+
+
+# In[29]:
+
+
+#.format method 
+
+
+# In[30]:
+
+
+#'string here {} {} {}'.format {something1, something2, something3}
+
+
+# In[31]:
+
+
+print('I am a student of {}'.format('Medhavi Skills University'))
+
+
+# In[33]:
+
+
+print('The {q} {b} {f}'.format(b='brown',f='fox',q='quick'))
+
+
+# In[ ]:
+
+
+
+
